29af367623ee8ec2bb5ff3a03da0ea04b2783089
Mohammad Zavvari <57547209+mohammadzavvari@users.noreply.github.com>
2023-12-27 14:10:41 +0330

    Debug Cassandra setting.

---
+++ src/test/kotlin/com/example/rtdatasystem/RtDataSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.rtdatasystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RtDataSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
