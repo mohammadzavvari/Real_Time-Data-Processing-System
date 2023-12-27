1a2c8f9812c629fefcb398d8fcaa3f241768c58c
Mohammad Zavvari <57547209+mohammadzavvari@users.noreply.github.com>
2023-12-28 01:45:40 +0330

    Add some changes to ProductService to call kafka producer to send messeges in case of creating and updating an object.

--- src/main/kotlin/com/example/rtdatasystem/service/ProductService.kt
+++ src/main/kotlin/com/example/rtdatasystem/service/ProductService.kt
@@ -4,16 +4,23 @@ import org.springframework.stereotype.Service
 
 import com.example.rtdatasystem.model.Product
 import com.example.rtdatasystem.repository.ProductRepository
+import com.example.rtdatasystem.service.serviceB.KafkaProductProducer
 import java.util.UUID
 
 @Service
-class ProductService(private val productRepository: ProductRepository) {
+class ProductService(
+    private val productRepository: ProductRepository,
+    private val kafkaProductProducer: KafkaProductProducer) {
 
     fun getAllProducts(): Iterable<Product> = productRepository.findAll()
 
     fun getProductById(productId: UUID): Product? = productRepository.findById(productId).orElse(null)
 
-    fun createProduct(product: Product): Product = productRepository.save(product)
+    fun createProduct(product: Product): Product{
+        val createdProduct = productRepository.save(product)
+        kafkaProductProducer.sendProduct(createdProduct)
+        return createdProduct
+    }
 
     fun updateProduct(existingProduct: Product, updatedProduct: Product): Product {
         existingProduct.name = updatedProduct.name
@@ -23,7 +30,9 @@ class ProductService(private val productRepository: ProductRepository) {
         existingProduct.category = updatedProduct.category
         existingProduct.customerId = updatedProduct.customerId
 
-        return productRepository.save(existingProduct)
+        val newProduct =  productRepository.save(existingProduct)
+        kafkaProductProducer.sendProduct(newProduct)
+        return newProduct
     }
 
 }
